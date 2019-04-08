407541c446d808723e415c346ae4f054c9908d53
Colton Lacey and Darrion Gering <colton@>
2019-04-08 14:23:21 -0700

    diplay list information upon clicking location

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ function DestinationList() {
 }
 
 DestinationList.prototype.addDestination = function(destination) {
+  destination.id = this.assignId();
   this.listOfDestinations.push(destination);
 }
 
@@ -23,23 +24,24 @@ function displayDestinations(listToDisplay) {
   var displayList = $("ul#destinations");
   var htmlDisplay = "";
   listToDisplay.listOfDestinations.forEach(function(destination) {
-  htmlDisplay += "<li id=" + destination.id + ">" + destination.location + " "  + + "</li>";
+  htmlDisplay += "<li id=" + destination.id + ">" + destination.location + " "  + "</li>";
   });
   displayList.html(htmlDisplay);
 }
 
 function displayProperties(listToDisplay) {
-  var displayList2 = $("ul#destinations");
-  var htmlDisplay = "<ul>";
+  var htmlDisplay2 = "";
   listToDisplay.listOfDestinations.forEach(function(destination) {
-  htmlDisplay2 += "<li id=" + destination.id + ">" + " " + destination.landmarks + destination.timeOfYear + "</li>";
-  });
-  htmlDisplay += "</ul>";
-  displayList2.html(htmlDisplay2);
+    var displayList2 = $("#properties");
+    htmlDisplay2 += "<ul>";
+    htmlDisplay2 += "<li class=" + "prop" + destination.id + ">" + " " + destination.landmarks + destination.timeOfYear + "</li> </ul>";
+    displayList2.html(htmlDisplay2);
+
+});
 }
 
 // User Interface
-$(document).ready(function() {
+$("document").ready(function() {
   var destinationList = new DestinationList();
   var destination1 = new Destination("Portland", ["Epicodus", "Moda Centeer", "Pioneer Square"], "Spring");
   var destination2 = new Destination("Seattle", ["Space Neddle", "Water Front", "Pike PLace"],  "Summer");
@@ -48,8 +50,15 @@ $(document).ready(function() {
   console.log(destinationList.listOfDestinations);
 
   displayDestinations(destinationList);
-  $(displayDestinations(destinationList)).toggle(function(){
-    $().toggle(displayProperties(destinationList));
+  displayProperties(destinationList);
+
+  $("#1").click(function(){
+    $(".prop1").toggle();
+    $("#2").toggle();
+  });
+  $("#2").click(function(){
+    $(".prop2").toggle();
+    $("#1").toggle();
   });
 
 });
